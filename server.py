c7910358931c4b5a04ecef2866deefc96fba53a9
Victor Lieu <victorlieu@Victors-MacBook-Air.local>
2022-01-25 15:20:51 -0700

    Updating server.py

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
+# Import os for handling path joins
+import os
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Victor Lieu
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,16 +28,94 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+# Resources used for this assignment:
+# https://docs.python.org/3/library/os.path.html
+
+
+# Define host url and port number
+HOST = "127.0.0.1"
+PORT = 8080
+BUFFER_SIZE = 1024
+
+"""
+Dictionary for handling certain HTTP status codes in HTTP1.1 for this assignment
+"""
+statusCodes = {
+    "OK": "HTTP/1.1 200 OK\r\n",
+    "moved": "HTTP/1.1 301 Moved Permanently\r\n",
+    "notFound": "HTTP/1.1 404 Not Found\r\n",
+    "notAllowed": "HTTP/1.1 405 Method Not Allowed\r\n"
+}
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
+    def handle_mimetype(self, path):
+        """
+        Function for checking the provided path and returning the mime_type
+        """
+        if path.endswith(".css"):
+            return "text/css"
+
+        return "text/html"
+
+    def send_request(self, status, path=None):
+        """
+        Function for sending content and HTTP status code responses
+        """
+        # Path was provided, send some additional content, otherwise just send status code
+        if path:
+            # Try and convert the path to a mimetype
+            try:
+                mime_type = self.handle_mimetype(path)
+                f = open(path, 'r+b')
+                content = f.read()
+                self.request.sendall(bytearray(statusCodes["OK"], 'utf-8'))
+                self.request.sendall(
+                    bytearray("Content-Type: {}\r\n".format(mime_type), 'utf-8'))
+                self.request.sendall(
+                    bytearray("Content-Length: {}\r\n".format(len(content)), 'utf-8'))
+                self.request.sendall(
+                    bytearray("Connection: close\r\n", 'utf-8'))
+                self.request.sendall(bytearray("\r\n", 'utf-8'))
+                self.request.sendall(content)
+                f.close()
+            except:
+                self.request.sendall(
+                    bytearray(statusCodes["notFound"], 'utf-8'))
+        else:
+            self.request.sendall(bytearray(statusCodes["notFound"], 'utf-8'))
+
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        # Responses should be in HTTP/1.1 since this is a 1.1 compliant web server
+        self.data = self.request.recv(BUFFER_SIZE).strip()
+        print("Got a request of: %s\n" % self.data)
+        # Split data and extract the request method and protocol
+        parsedData = self.data.split()
+        requestMethod, path = parsedData[0], parsedData[1]
+        print("Request Method: %s, Path: %s" % (requestMethod, path))
+
+        # Handle Get Requests
+        if requestMethod == "GET":
+            # Join path from current working directory, have it only serve files after ./www
+            joined_path = os.path.join(os.getcwd(), 'www' + path)
+            if not joined_path.endswith('/') and not os.path.isfile(joined_path):
+                joined_path += '/'
+                self.send_request("moved")
+            # Check that the absolute path of the joined path is www so no other directories are accessed
+            # if not os.path.abspath(joined_path).endswith('www'):
+            #     print("NOT FOUNDS")
+            #     self.send_request("notFound")
+            # else:
+            if not os.path.isfile(joined_path):
+                joined_path += 'index.html'
+            self.send_request("OK", joined_path)
+            # Return a 405 status code for other types of non-supported request methods (POST/PUT/DELETE)
+        else:
+            self.send_request("notAllowed")
+
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 8080
 
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
